3c74435eaed469d1c12e31714f87501a670508bd
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-01-24 19:35:37 -0500

    FIX

--- src/main/java/bdv/img/cache/loading/VolatileCacheLoader.java
+++ src/main/java/bdv/img/cache/loading/VolatileCacheLoader.java
@@ -2,5 +2,5 @@ package bdv.img.cache.loading;
 
 public interface VolatileCacheLoader< K, V > extends CacheLoader< K, V >
 {
-	public V createEmpty( final Long key );
+	public V createEmpty( final K key );
 }
